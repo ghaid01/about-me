93b411162fdc86793a2d0abf6b93c9c7ed538a84
ghaid01 <ghaid.zahran2001@gmail.com>
2019-10-07 15:46:17 +0300

    I've faced some issues with the lower and upper case but it worked after some tries

---
+++ app.js
@@ -0,0 +1,88 @@
+'use stripct'
+var name = prompt('What is your name ?');
+alert('Welcome ' + name + ' to my guessing game!!');
+var age = prompt('Do you think I\'m 18?');
+age = age.toLocaleLowerCase();
+if(age =='yes'){
+alert('Yes!! you are right!!');
+} else if(age =='y') {
+    alert('Yes!! you are right!!');
+}else if(age =='no'){
+    alert('oops you were wrong');
+
+    
+} else if(age =='n'){
+    alert('oops you were wrong');
+}
+else{
+    alert('guess again!');
+}
+
+
+var major = prompt('Do you think I study software engineering?');
+major = major.toLocaleLowerCase();
+console.log('major',major)
+if(major =='yes'){
+    alert('Yupp that is rightt!!');
+
+}
+else if(major == 'y'){
+    alert('Yupp that is rightt!!');
+}
+else if(major == 'no'){
+    alert('nope not true');
+}
+else if(major == 'n'){
+    alert('nope not true');
+} else {
+    alert('guess again!');
+}
+var hobbies = prompt('Do I like singing?');
+hobbies = hobbies.toLocaleLowerCase();
+if(hobbies=='yes'){
+    alert('Yes!! you are right!!');
+    } else if(hobbies =='y') {
+        alert('Yes!! you are right!!');
+    }else if(hobbies =='no'){
+        alert('oops you were wrong');
+    
+        
+    } else if(hobbies =='n'){
+        alert('oops you were wrong');
+    }
+    else{
+        alert('guess again!');
+    }
+var puppies = prompt('Do I like puppies?');
+puppies = puppies.toLocaleLowerCase();
+if(puppies=='yes'){
+    alert('Yupp that is rightt!!');
+
+}
+else if(puppies =='y'){
+    alert('Yupp that is rightt!!');
+}
+else if(puppies =='no'){
+    alert('nope not true');
+}
+else if(puppies =='n'){
+    alert('nope not true');
+} else {
+    alert('guess again!');
+}
+var color = prompt('Is my favorite color blue?');
+color = color.toLocaleLowerCase();
+if(color =='yes'){
+alert('Yes!! you are right!!');
+} else if(color =='y') {
+    alert('Yes!! you are right!!');
+}else if(color =='no'){
+    alert('oops you were wrong');
+
+    
+} else if(color =='n'){
+    alert('oops you were wrong');
+}
+else{
+    alert('guess again!');
+}
